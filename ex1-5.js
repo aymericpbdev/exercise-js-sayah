58637c05ab381ba2e13be3201e6ff18c3f99527a
aymericpbdev <pain_blavec@hotmail.fr>
2025-10-31 13:23:13 +0100

    ex1.js

---
+++ ex1-5.js
@@ -0,0 +1,8 @@
+// 1. Écrire un programme qui affiche dans la console les nombres de 1 à 10.
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// 2. Demander à l’utilisateur son prénom avec  prompt()  et afficher “Bonjour, prénom !” dans la console.
+
