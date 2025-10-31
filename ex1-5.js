5e1b305695bf3cddb83261dd9b0c21fdc641420d
aymericpbdev <pain_blavec@hotmail.fr>
2025-10-31 13:43:29 +0100

    ex4.js

--- ex1-5.js
+++ ex1-5.js
@@ -6,7 +6,7 @@ for (let i = 1; i <= 10; i++) {
 
 // 2. Demander à l’utilisateur son prénom avec  prompt()  et afficher “Bonjour, prénom !” dans la console.
 
-let prenom = prompt("Quel est votre prénom ?");
+let name = prompt("Quel est votre prénom ?");
 console.log("Bonjour," + prenom + " !");
 
 // 3. Calculer la somme de deux nombres saisis par l’utilisateur et afficher le résultat dans la console.
@@ -16,3 +16,9 @@ let secondNumber = prompt("Deuxième nombre de l'addition");
 let numberOne = Number(firstNumber);
 let numberTwo = Number(secondNumber);
 console.log(numberOne + numberTwo);
+
+// 4. Écrire une fonction qui prend une chaîne de caractères et retourne le nombre de caractères qu’elle contient.
+
+let word = prompt("Ecrivez un seul mot");
+let wordLength = word.length;
+console.log(wordLength + " lettres");
